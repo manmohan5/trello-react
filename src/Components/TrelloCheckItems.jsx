48369b169f15e792f437f2428109bb5656b21714
manmohan rathour <manmohancs27@gmail.com>
2020-01-13 16:49:13 +0530

    add line-through css on checkbox

--- src/Components/TrelloCheckItems.jsx
+++ src/Components/TrelloCheckItems.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-
 const TrelloCheckItems = props => {
-  const check=props.checked==='complete'?true:false
+  const check = props.checked === 'complete' ? true : false;
+
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <div>
-        <input type="checkbox"  checked={check} onChange={(e)=>props.handelChange(e.target.checked,props.checkItemID)}/>
-       
-        <span className="ml-1">{props.checkItemName}</span>
+        <input
+          type="checkbox"
+          checked={check}
+          onChange={e =>
+            props.handelChange(e.target.checked, props.checkItemID)
+          }
+        />
+
+        <span className="ml-1" style={{textDecoration: check ? 'line-through' : 'none'}}>
+          {props.checkItemName}
+        </span>
       </div>
 
       <button
